f2ab10a2edf42ec8a921f7f0d0b19d959cad79d8
Nahin-Nasir <nahinnasir21@gmail.com>
2021-03-02 08:32:30 +0600

    created bank class

--- First Assignment/Bank.cs
+++ First Assignment/Bank.cs
@@ -8,7 +8,75 @@ namespace First_Assignment
 {
     class Bank
     {
-       
+        private string bankName;
+        private Account[] myBank;
+
+        public Bank(string name, int size)
+        {
+            this.bankName = name;
+            myBank = new Account[size];
+        }
+        public string BankName
+        {
+            set { this.bankName = value; }
+            get { return this.bankName; }
+        }
+        public Account[] MyBank
+        {
+            set { this.myBank = value; }
+            get { return this.myBank; }
+        }
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < MyBank.Length; i++)
+            {
+                if (MyBank[i] == null)
+                {
+                    MyBank[i] = account;
+                    Console.WriteLine("Account Created");
+                    break;
+                }
+            }
+        }
+
+        public void DeleteAccount(int accountNumber)
+        {
+            for (int i = 0; i < MyBank.Length; i++)
+            {
+                if (MyBank[i].AccountNumber == accountNumber)
+                {
+                    MyBank[i] = null;
+                    Console.WriteLine("Account Deleted");
+                }
+
+            }
+
+        }
+        public void Transection(int transectionType)
+        {
+
+        }
+
+
+        public void PrintAccountDetailes()
+        {
+            for (int i = 0; i < MyBank.Length; i++)
+            {
+                if (MyBank[i] == null)
+                {
+                    continue;
+                }
+                MyBank[i].ShowAccountInformation();
+            }
+
+        }
+
+
+
 
     }
+
+
 }
+
